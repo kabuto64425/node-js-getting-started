6d52a4d5d75538b237a7953917e20f02572c494f
kabuto64425 <higasumi52@yahoo.co.jp>
2020-04-12 12:19:56 +0900

    aa

--- public/index.js
+++ public/index.js
@@ -1,8 +1,33 @@
 /* ----- option ----- */
 var txSp = 100; // テキストの表示速度
 var dly = 1000; // 次の文章までの待ち時間
-var postponement = 2 //ストップしてから文字を表示し続ける猶予文字
+var postponement = 2; //ストップしてから文字を表示し続ける猶予文字
 /* ----- option ----- */
+
+/* ----- quiz ----- */
+var quizzes = [
+    {
+      question: "1:「なぜ山に登るのか」と尋ねられて「そこに山があるから」と答えた逸話で知られる、イギリスの登山家は誰でしょう？",
+      answer: "ジョージ・マロリー"
+    },
+    {
+      question: "2:「なぜ山に登るのか」と尋ねられて「そこに山があるから」と答えた逸話で知られる、イギリスの登山家は誰でしょう？",
+      answer: "ジョージ・マロリー"
+    },
+    {
+      question: "3:「なぜ山に登るのか」と尋ねられて「そこに山があるから」と答えた逸話で知られる、イギリスの登山家は誰でしょう？",
+      answer: "ジョージ・マロリー"
+    },
+    {
+      question: "4:「なぜ山に登るのか」と尋ねられて「そこに山があるから」と答えた逸話で知られる、イギリスの登山家は誰でしょう？",
+      answer: "ジョージ・マロリー"
+    },
+    {
+      question: "5:「なぜ山に登るのか」と尋ねられて「そこに山があるから」と答えた逸話で知られる、イギリスの登山家は誰でしょう？",
+      answer: "ジョージ・マロリー"
+    }
+]
+/* ----- quiz ----- */
 
 var questionElement = document.getElementById('question');
 var answerElement = document.getElementById('answer');
@@ -12,16 +37,19 @@ var answerText;
 
 var questionDisplayingTextCount = 0;
 
+quizIndex = 0;
+
 window.onload = function(){
-  kamikakushi();
+  kamikakushi(quizzes[quizIndex]);
   itimozi(postponement);
+  quizIndex++;
 }
  
-function kamikakushi(){ // 要素を変数に保持させ、非表示にする
-  questionText = questionElement.firstChild.nodeValue;
+function kamikakushi(quiz){ // 要素を変数に保持させ、非表示にする
+  questionText = quiz.question;
   questionElement.textContent = '';
 
-  answerText = answerElement.firstChild.nodeValue;
+  answerText = quiz.answer;
   answerElement.textContent = '';
 }
 
@@ -54,17 +82,30 @@ Array.from(document.getElementsByTagName("a")).forEach(element => {
       answerElement.textContent = answerText;
       return;
     }
+    if(phase == PHASES.FULLVIEW) {
+      phase = PHASES.READINGQUESTION;
+      if(quizIndex >= quizzes.length) {
+        quizIndex = 0;
+      }
+
+      kamikakushi(quizzes[quizIndex]);
+      questionDisplayingTextCount = 0;
+      itimozi(postponement);
+      quizIndex++;
+      return;
+    }
   });
 });
 
 function itimozi(postponement){ //　一文字ずつ表示させる
   console.log(postponement);
-  questionElement.innerHTML = questionText.substr( 0, ++questionDisplayingTextCount ); // テキストの指定した数の間の要素を表示
-
-  if(postponement <= 0) {
+  
+  if(postponement < 0) {
     phase = PHASES.INANSWER;
     return;
   }
+  
+  questionElement.innerHTML = questionText.substr( 0, ++questionDisplayingTextCount ); // テキストの指定した数の間の要素を表示
 
   if(questionText.length != questionDisplayingTextCount){ // Count が初期の文字列の文字数と同じになるまでループ
     setTimeout(function() {
