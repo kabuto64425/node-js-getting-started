fdba5692d903bce133400e9ca8911222f3a8d88b
kabuto64425 <higasumi52@yahoo.co.jp>
2020-04-13 15:31:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const pg = require('pg')
 const PORT = process.env.PORT || 5000
 
 var pool = pg.Pool ({
-  host: process.env.ENV_HOST,
-  databese: process.env.ENV_DATABASE,
-  user: process.env.ENV_USER,
+  host: 'ec2-34-204-22-76.compute-1.amazonaws.com',
+  databese: 'd4t9vmtvadroi7',
+  user: 'jzcqnyvlmwovjp',
   port: 5432,
-  password: process.env.ENV_PASSWORD,
+  password: 'b249ce1d8b423de80a9ae5fe0bb6d588d0154d183a627265b0a24ebb07a3f51f',
 });
 
 pool.connect(function(err, client, done) {
