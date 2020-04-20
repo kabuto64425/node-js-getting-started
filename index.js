b7dd7699691b89c470df0662b089483a0672a113
kabuto64425 <higasumi52@yahoo.co.jp>
2020-04-20 17:49:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ io.on('connection', function (socket) {
     function itimozi(postponement){ //　一文字ずつ表示させる
       
       io.emit('sending message', questionText.substr( 0, ++questionDisplayingTextCount )); // テキストの指定した数の間の要素を表示
-    
-      if(questionText.length != questionDisplayingTextCount){ // Count が初期の文字列の文字数と同じになるまでループ
-        /*setTimeout(function() {
+      
+      if(questionDisplayingTextCount > questionText.length){ // Count が初期の文字列の文字数と同じになるまでループ
+        setTimeout(function() {
             itimozi(postponement);
         }, txSp); // 次の文字へ進む*/
       }
